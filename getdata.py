96d5c7a4fd5b3a1fe721ee2b3f5f2ebd14bc30a5
Oscar Cárdenas Gómez <44097770+Oscar6647@users.noreply.github.com>
2025-01-15 23:46:12 -0600

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -1,6 +1,6 @@
 from roboflow import Roboflow
 
-rf = Roboflow(api_key="C4ki0cbMqzmxzx4EQC25")
+rf = 
 project = rf.workspace("lab1-1djxq").project("lab_2-sm2mr")
 version = project.version(1)
 dataset = version.download("yolov8")
